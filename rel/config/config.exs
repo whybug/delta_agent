52635cd1a93beb9f4facd33cebf0b2c8756a40b6
Adrian Philipp <adrian@ticketswap.com>
2018-11-21 16:36:00 +0100

    Use different default url

--- rel/config/config.exs
+++ rel/config/config.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 config :delta_agent,
        version: System.get_env("APP_VERSION") || "dev",
-       api_host: System.get_env("API_HOST") || "https://delta-schema.whybug.com/api/usage/track",
+       api_host: System.get_env("API_HOST") || "https://delta-schema.whybug.com/api/usage/bulk",
        udp_port: String.to_integer(System.get_env("UDP_PORT") || "2135"),
        http_port: String.to_integer(System.get_env("HTTP_PORT") || "2135"),
        flush_interval_ms: String.to_integer(System.get_env("FLUSH_INTERVAL_MS") || "10000"),
