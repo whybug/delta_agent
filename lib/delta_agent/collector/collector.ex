71828e7f041ca6f2ccf18cad106c584b98a14f84
Adrian Philipp <adrian@ticketswap.com>
2018-11-28 14:18:48 +0100

    Switch to lists to make json encoding easy

--- lib/delta_agent/collector/collector.ex
+++ lib/delta_agent/collector/collector.ex
@@ -47,11 +47,11 @@ defmodule DeltaAgent.Collector do
           end)
       )
 
-    # todo: Is it worth to delete those? Might not be needed
+    # Is it worth to delete those? Might not be needed
     :ets.delete_all_objects(:operations)
     :ets.delete_all_objects(:aggregates)
 
-    # todo: Figure out how to prevent possible data loss between tab2list
+    # Figure out how to prevent possible data loss between tab2list
     # and delete_all_objects. Probably delete object (returns it) one by one.
     :ets.delete_all_objects(:usages)
 
@@ -63,7 +63,7 @@ defmodule DeltaAgent.Collector do
 
     :ets.insert_new(:operations, {operation.hash, operation.body})
     :ets.insert_new(:aggregates, {aggregate.hash, aggregate})
-    :ets.insert(:usages, {aggregate.hash, [{operation.timestamp, operation.metadata}]})
+    :ets.insert(:usages, {aggregate.hash, [[operation.timestamp, operation.metadata]]})
 
     {:ok}
   end
